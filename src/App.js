a41dc49a8519f016c8834a89c0d197636df8802d
Tamaghna Ghosh <ghoshtamaghna6991@gmail.com>
2022-11-08 16:50:31 +0530

    1st commmit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Button } from "react-bootstrap";
+
+function App() {
+  return (
+    <div className="App">
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
